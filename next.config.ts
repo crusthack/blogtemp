147cd8e41e8f0605ade3950c79c6232b4d384a8b
crusthack <hwangjs0823@naver.com>
2025-12-22 00:54:35 +0900

    dd

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { NextConfig } from 'next';
  
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  output: "export",
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
  
